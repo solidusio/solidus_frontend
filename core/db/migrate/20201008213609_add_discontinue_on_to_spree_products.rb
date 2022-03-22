1b0d517fa83b1144533b8d717cc589b965dc08da
George Mendoza <gsmendoza@gmail.com>
2022-03-22 15:16:25 +0800

    [BREAKS BUILD] Remove all Solidus components except for SolidusFrontend

--- core/db/migrate/20201008213609_add_discontinue_on_to_spree_products.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class AddDiscontinueOnToSpreeProducts < ActiveRecord::Migration[5.2]
-  def change
-    add_column :spree_products, :discontinue_on, :datetime
-  end
-end
