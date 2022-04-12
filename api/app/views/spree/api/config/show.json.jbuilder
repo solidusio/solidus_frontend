7b4299f83f0c8bc526ab512fd583c6847005d2f3
George Mendoza <gsmendoza@gmail.com>
2022-04-12 17:58:55 +0800

    [BREAK CI] Remove Solidus dependencies

--- api/app/views/spree/api/config/show.json.jbuilder
+++
@@ -1,3 +0,0 @@
-# frozen_string_literal: true
-
-json.default_country_iso(Spree::Config[:default_country_iso])
--- core/app/models/spree/image.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-module Spree
-  class Image < Asset
-    include ::Spree::Config.image_attachment_module
-  end
-end
