57b66c1efffe357c1b7d6c91ca06b6261afea4a2
Alberto Vena <kennyadsl@gmail.com>
2023-04-21 11:08:03 +0200

    Make specs compliant with new taxon factories
    In Solidus core we improved how taxon factories are built
    and we need to reflect that change in the frontend specs
    as well.
    
    Ref https://github.com/solidusio/solidus/pull/4851

--- spec/helpers/base_helper_spec.rb
+++ spec/helpers/base_helper_spec.rb
@@ -6,8 +6,9 @@ module Spree
   describe BaseHelper, type: :helper do
     # Regression test for https://github.com/spree/spree/issues/2759
     it "nested_taxons_path works with a Taxon object" do
-      taxon = create(:taxon, name: "iphone")
-      expect(spree.nested_taxons_path(taxon)).to eq("/t/iphone")
+      taxonomy = create(:taxonomy, name: "smartphones")
+      taxon = create(:taxon, name: "iphone", taxonomy: taxonomy)
+      expect(spree.nested_taxons_path(taxon)).to eq("/t/smartphones/iphone")
     end
   end
 end
